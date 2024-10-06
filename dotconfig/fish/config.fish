1181c7c6fcf257a6400c6b71afc6531c97065878
timofey basuev <echiauta@gmail.com>
2024-10-06 17:49:00 +0500

    fix: minor changes

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -3,24 +3,23 @@ set fish_greeting ""
 alias ls "lsd --group-directories-first --icon never"
 alias la "lsd -A --group-directories-first --icon never"
 alias ll "lsd -l --group-directories-first --icon never"
-alias lla "ll -A"
+alias lla "lsd -lA --group-directories-first --icon never"
 alias off "sudo shutdown -h now"
 alias n "nnn -CHde"
 alias du "du -hs * | sort -h"
 alias df "df -h"
-alias xbls "xbacklight -set "
-alias ngp "cd ~/mind && git add . && git commit -m 'fix: minor changes' && git push"
+alias xbs "xbacklight -set "
 alias webcam "mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
-alias muz "mpv --no-terminal --shuffle ."
+alias v "nvim "
 
-command -qv nvim
+alias ga "git add "
+alias gc "git commit -m "
 
-set -Ux EDITOR nvim
-set -Ux PATH bin $PATH
-set -Ux PATH ~/bin $PATH
-set -Ux PATH ~/.local/bin $PATH
-set -Ux _JAVA_AWT_WM_NONREPARENTING "1"
-set -Ux DOTNET_CLI_TELEMETRY_OPTOUT "1"
-set -Ux NPM_CONFIG_PREFIX "~/.npm-global"
-set -Ux PATH $PATH:~/.npm-global/bin
-set -Ux LIBVA_DRIVER_NAME "i965"
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH "$PATH:$(go env GOPATH)/bin"
+set -gx PATH $PATH ~/.npm-global/bin
+set -gx LIBVA_DRIVER_NAME "i965"
+
+starship init fish | source
