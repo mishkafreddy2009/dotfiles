369435ca9279612e92bd3b24cc69a8d898483880
timofey basuev <echiauta@gmail.com>
2024-11-08 16:12:16 +0500

    fish

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -1,8 +1,9 @@
 set fish_greeting ""
 
 set -gx EDITOR "nvim"
-set -gx BROWSER firefox
+set -gx BROWSER chromium
 
+set -gx GOPATH "$HOME/.go"
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
@@ -15,21 +16,13 @@ alias ls "lsd --group-directories-first --icon never"
 alias la "lsd -A --group-directories-first --icon never"
 alias ll "lsd -l --group-directories-first --icon never"
 alias lla "lsd -lA --group-directories-first --icon never"
-abbr off "sudo shutdown -h now"
+alias v "nvim"
+abbr off "doas shutdown -h now"
 abbr duh "du -hs * | sort -h"
 abbr xbs "xbacklight -set"
 abbr webcam "mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
-abbr v "nvim"
 
-abbr nekoray "/home/auta/src/nekoray/./launcher"
-
-abbr xbpsq "xbps-query -Rs"
-abbr xbpsi "sudo xbps-install -Sv"
-abbr xbpsu "sudo xbps-install -Suv"
-abbr xbpsc "sudo xbps-remove -Oov"
-abbr xbpsr "sudo xbps-remove -Rv"
-
-abbr mci "sudo make clean install"
+abbr mci "doas make clean install"
 
 function fish_user_key_bindings
 	bind \ec 'cd $(fd --type d --hidden --exclude .git --exclude node_module --exclude .cache --exclude .npm --exclude .mozilla --exclude .meteor --exclude .nv | fzf --layout reverse); commandline -f repaint'
